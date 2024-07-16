407180fd55b9b8c1727434b7124a08e61282957f
Akrem Hchaichi <hchaichi-akrem@outlook.fr>
2024-07-16 06:38:00 +0100

    add reset button & display users

--- frontend/src/users/components/Users.tsx
+++ frontend/src/users/components/Users.tsx
@@ -1,9 +1,9 @@
 
 import { useFieldArray, useFormContext, useWatch } from 'react-hook-form'
-import { userSchema } from '../types/schema'
-import { Button, Stack, Typography } from '@mui/material'
+import { defaultValues, userSchema } from '../types/schema'
+import { Button, Container, List, ListItem, ListItemButton, ListItemText, ListSubheader, Stack, Typography } from '@mui/material'
 import { RHFAutocomplete } from '../../components/RHFAutocomplete'
-import { useGenders, useLanguages, useSkills, useStates } from '../services/queries';
+import { useGenders, useLanguages, useSkills, useStates, useUser, useUsers } from '../services/queries';
 import { RHFToggleButtonGroup } from '../../components/RHFToggleButtonGroup';
 import { RHFRadioGroup } from '../../components/RHFRadioGroup';
 import { RHFCheckbox } from '../../components/RHFCheckbox';
@@ -20,15 +20,26 @@ export function Users() {
     const languagesQuery = useLanguages();
     const gendersQuery = useGenders();
     const skillsQuery = useSkills();
+    const usersQuery = useUsers();
 
-    const { register, control, unregister, formState: { errors } } = useFormContext<userSchema>()
+    const { register, control, unregister, reset, setValue, formState: { errors } } = useFormContext<userSchema>()
 
+    const id = useWatch({ control, name: 'id' });
     const isTeacher = useWatch({ control, name: 'isTeacher' });
+    const userQuery = useUser(id);
 
     const { append, fields, remove, replace } = useFieldArray({
         control,
         name: 'students',
     });
+
+    const handleReset = () => {
+        reset(defaultValues);
+    };
+
+    const handleUserClick = (id: string) => {
+        setValue('id', id);
+    };
 
     useEffect(() => {
         if (!isTeacher) {
@@ -37,63 +48,91 @@ export function Users() {
         }
     }, [isTeacher, replace, unregister]);
 
+    useEffect(() => {
+        if (userQuery.data) {
+            reset(userQuery.data);
+        }
+    }, [reset, userQuery.data]);
 
     return (
-        <Stack sx={{ gap: 2 }}>
-            <RHFTextField<userSchema> name="name" label="Name" />
-            <RHFTextField<userSchema> name="email" label="Email" />
-            <RHFAutocomplete<userSchema>
-                name="states"
-                label="States"
-                options={statesQuery.data}
-            />
-            <RHFToggleButtonGroup<userSchema>
-                name="languagesSpoken"
-                options={languagesQuery.data}
-            />
-            <RHFRadioGroup<userSchema>
-                name="gender"
-                options={gendersQuery.data}
-                label="Gender"
-            />
-            <RHFCheckbox<userSchema>
-                name="skills"
-                options={skillsQuery.data}
-                label="Skills"
-            />
-            <RHFDateTimePicker<userSchema>
-                name="registrationDateAndTime"
-                label="Registration Date & Time"
-            />
-            <Typography>Former Employment Period:</Typography>
-            <RHFDateRangePicker<userSchema> name="formerEmploymentPeriod" />
-            <RHFSlider<userSchema> name="salaryRange" label="Salary Range" />
-            <RHFSwitch<userSchema> name="isTeacher" label="Are you a teacher?" />
+        <Container maxWidth="sm" component="form" >
+            <Stack sx={{ flexDirection: 'row', gap: 2 }}>
 
-            {isTeacher && (
-                <Button onClick={() => append({ name: '' })} type="button">
-                    Add new student
-                </Button>
-            )}
-
-            {fields.map((field, index) => (
-                <Fragment key={field.id}>
-                    <RHFTextField<userSchema>
-                        name={`students.${index}.name`}
-                        label="Name"
+                <List subheader={<ListSubheader>Users</ListSubheader>}>
+                    {usersQuery.data?.map((user) => (
+                        <ListItem disablePadding key={user.id}>
+                            <ListItemButton
+                                onClick={() => handleUserClick(user.id)}
+                                selected={id === user.id}
+                            >
+                                <ListItemText primary={user.label} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Stack sx={{ gap: 2 }}>
+                    <RHFTextField<userSchema> name="name" label="Name" />
+                    <RHFTextField<userSchema> name="email" label="Email" />
+                    <RHFAutocomplete<userSchema>
+                        name="states"
+                        label="States"
+                        options={statesQuery.data}
                     />
-                    <Button
-                        color="error"
-                        onClick={() => {
-                            remove(index);
-                        }}
-                        type="button"
-                    >
-                        Remove
-                    </Button>
-                </Fragment>
-            ))}
+                    <RHFToggleButtonGroup<userSchema>
+                        name="languagesSpoken"
+                        options={languagesQuery.data}
+                    />
+                    <RHFRadioGroup<userSchema>
+                        name="gender"
+                        options={gendersQuery.data}
+                        label="Gender"
+                    />
+                    <RHFCheckbox<userSchema>
+                        name="skills"
+                        options={skillsQuery.data}
+                        label="Skills"
+                    />
+                    <RHFDateTimePicker<userSchema>
+                        name="registrationDateAndTime"
+                        label="Registration Date & Time"
+                    />
+                    <Typography>Former Employment Period:</Typography>
+                    <RHFDateRangePicker<userSchema> name="formerEmploymentPeriod" />
+                    <RHFSlider<userSchema> name="salaryRange" label="Salary Range" />
+                    <RHFSwitch<userSchema> name="isTeacher" label="Are you a teacher?" />
 
-        </Stack>
+                    {isTeacher && (
+                        <Button onClick={() => append({ name: '' })} type="button">
+                            Add new student
+                        </Button>
+                    )}
+
+                    {fields.map((field, index) => (
+                        <Fragment key={field.id}>
+                            <RHFTextField<userSchema>
+                                name={`students.${index}.name`}
+                                label="Name"
+                            />
+                            <Button
+                                color="error"
+                                onClick={() => {
+                                    remove(index);
+                                }}
+                                type="button"
+                            >
+                                Remove
+                            </Button>
+                        </Fragment>
+                    ))}
+
+                    <Stack sx={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Button type="submit">
+                            New user
+                        </Button>
+                        <Button onClick={handleReset}>Reset</Button>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Container >
     )
 }
--- frontend/src/users/services/queries.ts
+++ frontend/src/users/services/queries.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 import { useQuery } from '@tanstack/react-query';
 import { Option } from '../../types/option';
+import { ApiGet } from '../../types/apiTypes';
+import { userSchema } from '../types/schema';
 
 
 export function useStates() {
@@ -41,5 +43,50 @@ export function useSkills() {
             axios
                 .get<Option[]>('http://localhost:8080/skills')
                 .then((res) => res.data),
+    });
+}
+
+export function useUsers() {
+    return useQuery({
+        queryKey: ['users'],
+        queryFn: (): Promise<Option[]> =>
+            axios.get<ApiGet[]>('http://localhost:8080/users').then((response) =>
+                response.data.map((user) => ({
+                    id: user.id.toString(),
+                    label: user.name,
+                }))
+            ),
+    });
+}
+
+
+export function useUser(id: string) {
+    return useQuery({
+        queryKey: ['user', { id }],
+        queryFn: async (): Promise<userSchema> => {
+            const { data } = await axios.get<ApiGet>(
+                `http://localhost:8080/users/${id}`
+            );
+
+            return {
+                variant: 'edit',
+                id: data.id.toString(),
+                name: data.name,
+                email: data.email,
+                formerEmploymentPeriod: [
+                    new Date(data.formerEmploymentPeriod[0]),
+                    new Date(data.formerEmploymentPeriod[1]),
+                ],
+                gender: data.gender,
+                languagesSpoken: data.languagesSpoken,
+                registrationDateAndTime: new Date(data.registrationDateAndTime),
+                salaryRange: [data.salaryRange[0], data.salaryRange[1]],
+                skills: data.skills,
+                states: data.states,
+                students: data.students,
+                isTeacher: data.isTeacher,
+            };
+        },
+        enabled: !!id,
     });
 }
